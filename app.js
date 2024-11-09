8c20dc2532ae2fa6f7399ca153af8d094c5e3bad
danielaryass <daniel.aryass7@gmail.com>
2024-11-09 18:12:53 +0700

    [CHORE] add cors

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
 const menuController = require('./menuController')
+const cors = require('cors');
+app.use(cors({
+  origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+}));
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+// set cors all allows
 app.use(bodyParser.json())
 
 app.post("/menus", menuController.createMenu);
